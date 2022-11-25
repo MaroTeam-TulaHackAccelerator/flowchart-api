818453ced4d9dfb2240c1f036784f79c8cb7be30
Christian Guetnga <lsdrfrx@gmail.com>
2022-11-26 00:17:20 +0300

    added docker-compose.yaml

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import mongoose from 'mongoose'
 
 async function bootstrap() {
+  await mongoose.connect('mongodb://localhost:27017')
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
