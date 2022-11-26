c92f1bfa3973f4c2126bdc79b3f71c4df4898b0c
Christian Guetnga <lsdrfrx@gmail.com>
2022-11-26 16:29:43 +0300

    modified docker-compose.yaml

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import mongoose from 'mongoose'
 
 async function bootstrap() {
-  await mongoose.connect('mongodb://localhost:27017')
+  // await mongoose.connect('mongodb://admin:admin@localhost:27017')
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
