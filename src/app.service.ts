d821aab33920c43dcb12a6115186ac14d4849dbd
Christian Guetnga <lsdrfrx@gmail.com>
2022-11-27 02:59:06 +0300

    optimized imports

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { AuthWorker } from 'src/auth/auth';
 import { Room, RoomDocument } from './schemas/room.schema';
 
 @Injectable()
