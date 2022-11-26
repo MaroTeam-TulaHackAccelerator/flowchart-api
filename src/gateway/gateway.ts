f412aca12f0f0f60841ca1d04f53772ef2cd7c4d
Alferov Arkady <arhe414@gmail.com>
2022-11-26 15:42:37 +0300

    Fix_Added the creation of rooms, changed the name of the variables

--- src/gateway/gateway.ts
+++ src/gateway/gateway.ts
@@ -8,7 +8,7 @@ import {
 } from "@nestjs/websockets";
 
 import { Server, Socket } from 'socket.io';
-import { IGoJsModel } from 'src/models/igo-Js.model';
+import { NewMessageDto } from 'src/dto/new-message.dto';
 
 @WebSocketGateway()
 export class AppGateway implements OnModuleInit {
@@ -17,29 +17,38 @@ export class AppGateway implements OnModuleInit {
   private logger: Logger = new Logger('AppGateway');
 
   @SubscribeMessage('newMessage')
-  onNewMessage(@MessageBody() body: any) {
+  onNewMessage(@MessageBody() body: NewMessageDto) {
     this.logger.log(body);
-    this.server.to(body.conn).emit('onMessage', {
+    this.server.to(body.roomId).emit('onMessage', {
       message: "New Message",
-      content: body.msg
+      content: body.model
     })
   }
   
   @SubscribeMessage('joinToRoom')
-  joinToRoom(@MessageBody() body: string, @ConnectedSocket() client: Socket){
-    this.logger.log(body);
-    client.join(body)
-    this.logger.log(`Client ${client.id}, joined to room: ${client.rooms.has(body) ? body : undefined}`);
-    this.server.to(body).emit('onJoin', {
+  joinToRoom(@MessageBody() roomId: string, @ConnectedSocket() client: Socket){
+    this.logger.log(roomId);
+    client.join(roomId)
+    this.logger.log(`Client ${client.id}, joined to room: ${roomId}`);
+    this.server.to(roomId).emit('onJoin', {
       message: `Client ${client.id} has been joined`
     });
   }
 
+  @SubscribeMessage('createRoom')
+  createRoom(@MessageBody() roomId: string, @ConnectedSocket() client: Socket){
+    client.join(`${roomId}`);
+    this.logger.log((roomId));
+    this.logger.log(`Client ${client.id}, created room: ${roomId}`);
+    this.server.to(roomId).emit('onCreate', {
+      message: `Client ${client.id}, created room: ${roomId}`
+    })
+  }
+
   onModuleInit() {
     this.server.on('connection', socket => {
-      socket.join(`${socket.id}`);
       this.logger.log((socket.id));
-      this.logger.log(`Client ${socket.id}, joined to room: ${socket.rooms.has(socket.id) ? socket.id : undefined}`);
+      this.logger.log(`Client ${socket.id} has been connected`);
     })
   }
 }
--- src/models/go-Js.model.ts
+++ src/models/go-Js.model.ts
@@ -1,7 +1,7 @@
 import { LinkDataModel } from "./link-data.model";
 import { NodeDataModel } from "./node-data.model";
 
-export interface IGoJsModel{
+export class GoJsModel{
     class: string;
     nodeDataArray: NodeDataModel[];
     linkDataArray: LinkDataModel[];
