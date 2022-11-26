c75a44ca3883b72d99f834d6e647d709dcfe8f8a
Christian Guetnga <lsdrfrx@gmail.com>
2022-11-26 18:02:41 +0300

    implemented connection to mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { GatewayModule } from './gateway/gateway.module';
 
+const DB_URI = process.env.DB_URI ? process.env.DB_URI : 'mongodb://admin:admin@localhost:27017'
+
 @Module({
-  imports: [GatewayModule],
+  imports: [GatewayModule, MongooseModule.forRoot(DB_URI)],
   controllers: [AppController],
   providers: [AppService]
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-const DB_URI = process.env.DB_URI ? process.env.DB_URI : 'mongodb://admin:admin@localhost:27017'
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
