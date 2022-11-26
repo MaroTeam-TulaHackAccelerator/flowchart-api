f6d4cd4f92c1700eeed2f837754d348b8c7622de
Christian Guetnga <lsdrfrx@gmail.com>
2022-11-27 02:09:04 +0300

    added getUserProjects endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,5 @@
-import { Controller } from '@nestjs/common';
-import { AppGateway } from './gateway/gateway';
+import { Controller, Get, Headers } from '@nestjs/common';
+import { AuthService } from './auth/auth.service';
 
 @Controller()
 export class AppController {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,16 +2,20 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { JwtService } from '@nestjs/jwt';
 import { Model } from 'mongoose';
-import { User, UserDocument, UserSchema } from './schemas/user.schema';
+import { User, UserDocument } from './schemas/user.schema';
+import { Room, RoomDocument } from 'src/gateway/schemas/room.schema';
 
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-  constructor(@InjectModel(User.name) private readonly model: Model<UserDocument>, private jwtService: JwtService) {}
+  constructor(
+    @InjectModel(User.name) private readonly userModel: Model<UserDocument>,
+    @InjectModel(Room.name) private readonly roomModel: Model<RoomDocument>,
+    private jwtService: JwtService) {}
 
   async createUser(login: string, email: string, password: string) {
-    const user = await this.model.create({
+    const user = await this.userModel.create({
       login: login,
       email: email,
       password: await bcrypt.hash(password, 7),
@@ -21,7 +25,7 @@ export class AuthService {
   }
 
   async validateUser(login: string, password: string) {
-    const user = await this.model.findOne({login: login}).exec();
+    const user = await this.userModel.findOne({login: login}).exec();
     if (!user) {
       return null;
     }
@@ -37,11 +41,21 @@ export class AuthService {
   generateToken(user: any) {
     const payload = {
       login: user.login,
-      sub: user._id,
+      userid: user._id,
     }
 
     return {
       access_token: this.jwtService.sign(payload),
     }
+  }
+
+  parseToken(token: string) {
+    const user = this.jwtService.verify(token)
+    return user
+  }
+
+  async getUserProjects(userid: string) {
+    const projects = await this.roomModel.find({userId: userid})
+    return projects
   }
 }
